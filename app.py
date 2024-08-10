eb213e36dda7425ef92c33ff120ce7a2994b8cbb
Rachit Goyal <rachit.promo@gmail.com>
2024-08-10 12:48:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return "Testing Sucessful"
 
+@app.l1('/')
+def l1():
+    return "How about one more 'Hello World!'"
+
 if __name__ == '__main__':
     app.run(debug=True)
